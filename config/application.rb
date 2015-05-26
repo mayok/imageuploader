fa9dae1bd61ad57dbe1b7b845604dc07aab3dfed
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-05-26 20:30:26 +0900

    picture_uploader.rb を読み込んでくれるようにした

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ImageUpoader
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # CarrierWave
+    config.autoload_paths += %W(#{config.root}/app/uploaders)
   end
 end
