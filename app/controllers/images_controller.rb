6acddad9b833f657cb586e47780b24308158d3ec
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-05-26 20:20:29 +0900

    Image controller の create 実装した

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,5 +4,19 @@ class ImagesController < ApplicationController
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:success] = "Successfully files uploaded."
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def image_params
+      params.require(:image).permit(:title, :caption)
+    end
+
 end
