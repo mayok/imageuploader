105456946bc72d81209f33e889313afcc4f51c47
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-05-30 19:12:53 +0900

    画像は必須

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  validates :picture, presence: true
 
   def pictures
     Image.all
